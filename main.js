1b4b84b52e881f2c53b8483e2a236784900fe739
FarymaDm <49679229+FarymaDm@users.noreply.github.com>
2019-04-21 22:50:35 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
-var a = 9,
-	b = 9;
-function createMatrix(rows, columns) {
-	var arr = new Array();
-	for (var i = 1; i <= rows; i++) {
-		arr[i] = new Array();
-		for (var j = 1; j <= columns; j++) {
-			arr[i][j] = i * j;
-		}
-	}
-	return arr;
-}
-var myArr = createMatrix(a, b);
-for (var i = 1; i <= a; i++) {
-	var p_cell = document.createElement('P');
-	for (var j = 1; j <= b; j++) {
-		var p_cell = document.createElement('P');
-		if (i == j) {
-			p_cell.classList.add('green');
-		} else if (i > j) {
-			p_cell.classList.add('blue');
-		} else {
-			p_cell.classList.add('yellow');
-		}
-		p_cell.innerHTML = myArr[i][j];
-		var cell = document.getElementById('main').appendChild(p_cell);
-	}
-}
+
+cols = 9;
+rows = 9;
+document.write(' <table border=2, cellpadding=10, cellspacing=0, width="70%">');
+for (i = 1; i <= cols; i++) 
+    {
+        document.write("<tr>");
+        for (k = 1; k <= rows; k++)
+         {
+ 
+             if (i == k) {
+                 document.write('<td style="background-color:green">');
+                 document.write(i * k);
+                
+             }
+             else
+                 if (i > k) {
+                     document.write('<td style="background-color:skyblue">');
+                     document.write(i * k);
+                     
+ 
+                 }
+                 else {
+                     document.write('<td style="background-color:gold">');
+                     document.write(i * k);
+                    
+                 }
+ 
+                 document.write("</td>");
+         }
+   
+        document.write("</tr>");
+    }
+document.write("</table>");
